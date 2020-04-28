a11cb814c9388a9eb0f276102fc6dfdf04fb8d26
Abiodun Sulaiman (FullStack Developer) <hello@abiodun.dev>
2020-04-28 11:41:42 +0100

    Implemented first SwiftUI State

--- Bulls Eye/ContentView.swift
+++ Bulls Eye/ContentView.swift
@@ -9,8 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsVisible: Bool = false
+    @State var whoIsThereIsVisible: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, BRYTA!")
+                .fontWeight(.bold)
+                .foregroundColor(Color.blue)
+            Button(action: {
+                print("Button pressed")
+                self.alertIsVisible = true
+            }) {
+                Text(/*@START_MENU_TOKEN@*/"Click me"/*@END_MENU_TOKEN@*/)
+            }
+            .alert(isPresented: $alertIsVisible) {
+                () -> Alert in
+                return Alert(title: Text("Hello from BRYTA"), message: Text("Making an alert"), dismissButton: .default(Text("Awesome")))
+            }
+            Button(action: {
+                self.whoIsThereIsVisible = true
+            }) {
+                Text(/*@START_MENU_TOKEN@*/"Knock, knock"/*@END_MENU_TOKEN@*/)
+            }
+            .alert(isPresented: $whoIsThereIsVisible) {
+                () -> Alert in
+                return Alert(title: Text("Who goes there?"), message: Text("It's Ryta"), dismissButton: .default(Text("Great")))
+            }
+        }
     }
 }
 
